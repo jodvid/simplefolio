89a0b616e7f3dbffef65f9c4fc07e3e5276a94b8
jo david <jo.wbdev@gmail.com>
2022-01-18 14:21:05 +0800

    updating new file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import initScrollReveal from "./scripts/scrollReveal";
 import initTiltEffect from "./scripts/tiltAnimation";
+import  "./Typing/Type.js";
 import { targetElements, defaultProps } from "./data/scrollRevealConfig";
+
+
 
 initScrollReveal(targetElements, defaultProps);
 initTiltEffect();
+
